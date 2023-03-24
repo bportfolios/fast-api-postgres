aca49c39a78ec1df114827e0bccf380e17ae8747
Bipika <bpka1990@gmail.com>
2023-03-24 12:42:37 +0200

    router creation from endpoint

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from fastapi import FastAPI
+from typing import Optional, List
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
 
-app = FastAPI()
+from api import users, courses, sections
 
+app = FastAPI(
+    title="Fast API LMS",
+    description="LMS for managing students and courses.",
+    version="0.0.1",
+    contact={
+        "name": "Gwen",
+        "email": "gwen@example.com",
+    },
+    license_info={
+        "name": "MIT",
+    },
+)
 
-@app.get("/users")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/users")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(users.router)
+app.include_router(courses.router)
+app.include_router(sections.router)

d0633327e9bb3c5faccb1c212a1bb8a1bf822a13
Bipika <bpka1990@gmail.com>
2023-03-24 12:22:55 +0200

    initial api setup

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/users")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/users")
+async def root():
+    return {"message": "Hello World"}
